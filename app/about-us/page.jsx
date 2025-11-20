5b49d38175271dec646ca6d41a454b4a770823a7
latif83 <latifm8360@gmail.com>
2025-11-20 10:15:51 +0000

    Updates:

--- app/about-us/page.jsx
+++ app/about-us/page.jsx
@@ -91,104 +91,104 @@ export default function AboutUs() {
 
                 {/* Placeholder team cards (you can replace with real team data later) */}
                 <div className="mt-8 grid md:grid-cols-3 gap-6 max-w-5xl mx-auto">
-                    <div
-                        className="bg-white rounded-xl shadow-md hover:shadow-lg transition-all duration-300 p-4 flex flex-col items-center"
-                    >
-                        <div className="w-24 h-24 rounded-full border mb-4"><Image src={'/woman_avatar.png'} width={500} height={500} className="w-full h-full" alt="Avatar Image" /></div>
-                        <h3 className="font-semibold text-[#0d4785]">Testimony Chukwu
-                        </h3>
-                        <p className="text-lg text-gray-900 font-bold">CEO</p>
-
-                        <Link
-                            href="tel:+905338479200" // Use the 'tel:' protocol for clicking to call
-                            className="text-sm text-gray-600 flex items-center gap-2 hover:text-blue-600 transition" // Added hover effect for UX
-                            title="Call now"
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
-                                <path fillRule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clipRule="evenodd" />
-                            </svg>
-                            <span>+90533 847 92 00</span>
-                        </Link>
-
-                        <Link
-                            href="mailto:testimonychukwu199@gmail.com" // Use the 'mailto:' protocol for clicking to email
-                            className="text-sm text-gray-600 flex items-center gap-2 hover:text-blue-600 transition"
-                            title="Send email"
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
-                                <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
-                                <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
-                            </svg>
-                            <span>testimonychukwu199@gmail.com</span>
-                        </Link>
-
-                    </div>
-
-                    <div
-                        className="bg-white rounded-xl shadow-md hover:shadow-lg transition-all duration-300 p-4 flex flex-col items-center"
-                    >
-                        <div className="w-24 h-24 rounded-full border mb-4"><Image src={'/woman_avatar.png'} width={500} height={500} className="w-full h-full" alt="Avatar Image" /></div>
-                        <h3 className="font-semibold text-[#0d4785]">Winifred Azuaru
-                        </h3>
-                        <p className="text-lg text-gray-900 font-bold">Creative Manager</p>
-
-                        <Link
-                            href="tel:+905338665214" // Use the 'tel:' protocol for clicking to call
-                            className="text-sm text-gray-600 flex items-center gap-2 hover:text-blue-600 transition" // Added hover effect for UX
-                            title="Call now"
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
-                                <path fillRule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clipRule="evenodd" />
-                            </svg>
-                            <span>+90 533 866 5214</span>
-                        </Link>
-
-                        <Link
-                            href="mailto:winifredc64@gmail.com" // Use the 'mailto:' protocol for clicking to email
-                            className="text-sm text-gray-600 flex items-center gap-2 hover:text-blue-600 transition"
-                            title="Send email"
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
-                                <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
-                                <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
-                            </svg>
-                            <span>winifredc64@gmail.com</span>
-                        </Link>
-
-                    </div>
-
-                    <div
-                        className="bg-white rounded-xl shadow-md hover:shadow-lg transition-all duration-300 p-4 flex flex-col items-center"
-                    >
-                        <div className="w-24 h-24 rounded-full border mb-4"><Image src={'/woman_avatar.png'} width={500} height={500} className="w-full h-full" alt="Avatar Image" /></div>
-                        <h3 className="font-semibold text-[#0d4785]">Mrs Gibson Ime
-                        </h3>
-                        <p className="text-lg text-gray-900 font-bold">Recruitment Consultant</p>
-
-                        <Link
-                            href="tel:+905338665214" // Use the 'tel:' protocol for clicking to call
-                            className="text-sm text-gray-600 flex items-center gap-2 hover:text-blue-600 transition" // Added hover effect for UX
-                            title="Call now"
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
-                                <path fillRule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clipRule="evenodd" />
-                            </svg>
-                            <span>+90 533 866 5214</span>
-                        </Link>
-
-                        <Link
-                            href="mailto:gibsonime@teamacgloballtd.com" // Use the 'mailto:' protocol for clicking to email
-                            className="text-sm text-gray-600 flex items-center gap-2 hover:text-blue-600 transition"
-                            title="Send email"
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
-                                <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
-                                <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
-                            </svg>
-                            <span>gibsonime@teamacgloballtd.com</span>
-                        </Link>
-
-                    </div>
+                     <div
+                                className="bg-white rounded-xl shadow-md hover:shadow-lg transition-all duration-300 p-4 flex flex-col items-center"
+                              >
+                                <div className="w-24 h-24 rounded-full border mb-4 p-3"><Image src={'/man_avatar.png'} width={500} height={500} className="w-full h-full" alt="Avatar Image" /></div>
+                                <h3 className="font-semibold text-[#0d4785]">Testimony Chukwu
+                                </h3>
+                                <p className="text-lg text-gray-900 font-bold">CEO</p>
+                    
+                                <Link
+                                  href="tel:+905338479200" // Use the 'tel:' protocol for clicking to call
+                                  className="text-sm text-gray-600 flex items-center gap-2 hover:text-blue-600 transition" // Added hover effect for UX
+                                  title="Call now"
+                                >
+                                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+                                    <path fillRule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clipRule="evenodd" />
+                                  </svg>
+                                  <span>+90 533 847 92 00</span>
+                                </Link>
+                    
+                                <Link
+                                  href="mailto:testimonychukwu199@gmail.com" // Use the 'mailto:' protocol for clicking to email
+                                  className="text-sm text-gray-600 flex items-center gap-2 hover:text-blue-600 transition"
+                                  title="Send email"
+                                >
+                                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+                                    <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+                                    <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+                                  </svg>
+                                  <span>info@teamacgloballtd.com</span>
+                                </Link>
+                    
+                              </div>
+                    
+                              <div
+                                className="bg-white rounded-xl shadow-md hover:shadow-lg transition-all duration-300 p-4 flex flex-col items-center"
+                              >
+                                <div className="w-24 h-24 rounded-full border mb-4"><Image src={'/woman_avatar.png'} width={500} height={500} className="w-full h-full" alt="Avatar Image" /></div>
+                                <h3 className="font-semibold text-[#0d4785]">Winifred Azuaru
+                                </h3>
+                                <p className="text-lg text-gray-900 font-bold">Creative Manager</p>
+                    
+                                <Link
+                                  href="tel:+905338665214" // Use the 'tel:' protocol for clicking to call
+                                  className="text-sm text-gray-600 flex items-center gap-2 hover:text-blue-600 transition" // Added hover effect for UX
+                                  title="Call now"
+                                >
+                                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+                                    <path fillRule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clipRule="evenodd" />
+                                  </svg>
+                                  <span>+90 533 866 5214</span>
+                                </Link>
+                    
+                                <Link
+                                  href="mailto:winifredc64@gmail.com" // Use the 'mailto:' protocol for clicking to email
+                                  className="text-sm text-gray-600 flex items-center gap-2 hover:text-blue-600 transition"
+                                  title="Send email"
+                                >
+                                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+                                    <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+                                    <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+                                  </svg>
+                                  <span>winifredc64@gmail.com</span>
+                                </Link>
+                    
+                              </div>
+                    
+                              <div
+                                className="bg-white rounded-xl shadow-md hover:shadow-lg transition-all duration-300 p-4 flex flex-col items-center"
+                              >
+                                <div className="w-24 h-24 rounded-full border mb-4"><Image src={'/woman_avatar.png'} width={500} height={500} className="w-full h-full" alt="Avatar Image" /></div>
+                                <h3 className="font-semibold text-[#0d4785]">Mrs Gibson Ime
+                                </h3>
+                                <p className="text-lg text-gray-900 font-bold">Recruitment Consultant</p>
+                    
+                                <Link
+                                  href="tel:+2348061321458" // Use the 'tel:' protocol for clicking to call
+                                  className="text-sm text-gray-600 flex items-center gap-2 hover:text-blue-600 transition" // Added hover effect for UX
+                                  title="Call now"
+                                >
+                                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+                                    <path fillRule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clipRule="evenodd" />
+                                  </svg>
+                                  <span>+234 806 132 1458</span>
+                                </Link>
+                    
+                                <Link
+                                  href="mailto:gibsonime@teamacgloballtd.com" // Use the 'mailto:' protocol for clicking to email
+                                  className="text-sm text-gray-600 flex items-center gap-2 hover:text-blue-600 transition"
+                                  title="Send email"
+                                >
+                                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+                                    <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+                                    <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+                                  </svg>
+                                  <span>gibsonime@teamacgloballtd.com</span>
+                                </Link>
+                    
+                              </div>
 
                 </div>
             </div>
